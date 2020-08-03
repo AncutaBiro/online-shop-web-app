a46af2831166c448910f7383f92eed5cbf4ecdd3
AncutaBiro <ancuta.biro@yahoo.com>
2020-08-03 18:51:34 +0300

    displaying products from API

--- js/shop.js
+++ js/shop.js
@@ -8,14 +8,38 @@ window.Shop = {
             URL: Shop.API_URL + "/products"
         }).done(function (response) {
             console.log(response);
+            // const responseJson = JSON.parse(response);
+            Shop.displayProducts(response.content);
         })
     },
 
     getProductHtml: function (product) {
-        return ` 
-        
-        
-        `
+        return `
+        <div class="col-md-3 col-sm-6">
+                <div class="single-shop-product">
+                    <div class="product-upper">
+                        <img src="img/product-2.jpg" alt="">
+                    </div>
+                    <h2><a href="">${product.name}</a></h2>
+                    <div class="product-carousel-price">
+                        <ins>$${product.price}</ins>
+                    </div>
+
+                    <div class="product-option-shop">
+                        <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id=${product.id}
+                           rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                    </div>
+                </div>
+         </div>
+                `
+    },
+
+    displayProducts: function (products) {
+        let productsHtml = '';
+
+        products.forEach(product => productsHtml += Shop.getProductHtml(product));
+
+        $('.single-shop-product .row:first-child').html(productsHtml);
     }
 };
 
